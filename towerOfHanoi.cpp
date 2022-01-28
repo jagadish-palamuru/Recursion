68a725f2aaf94f66f0cad58949a892a8c911765f
jagadish-palamuru <jagadishpalamuru@gmail.com>
2022-01-28 16:10:43 +0530

    tower

---
+++ towerOfHanoi.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void tower(int n, char src, char dest, char help)
+{
+    if(n==0)
+        return;
+    tower(n-1,src,help,dest);
+    cout<<"Move from "<<src<<" to "<<dest<<endl;
+    tower(n-1,help,dest,src);
+}
+int main()
+{
+    tower(3,'A','C','B');
+    return 0;
+}
